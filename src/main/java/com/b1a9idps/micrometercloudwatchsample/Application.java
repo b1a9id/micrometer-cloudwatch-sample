106770dea98b9904329057214657f452e30ce93c
Ryosuke Uchitate <ruchitate@coiney.com>
2019-04-13 12:25:24 +0900

    不要な依存関係を削除

--- src/main/java/com/b1a9idps/micrometercloudwatchsample/Application.java
+++ src/main/java/com/b1a9idps/micrometercloudwatchsample/Application.java
@@ -2,11 +2,6 @@ package com.b1a9idps.micrometercloudwatchsample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.http.converter.cbor.MappingJackson2CborHttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application {
